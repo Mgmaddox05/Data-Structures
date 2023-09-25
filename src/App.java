47f01c98c14194771aae076db729a4153e60a1d8
Mgmaddox05 <125920278+Mgmaddox05@users.noreply.github.com>
2023-09-25 14:59:03 -0500

    ffff

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        HashMap<Integer, Student> students = new HashMap<>();
+        int choice = 1;
+
+        while(choice != 5)
+        {
+            System.out.println("Student Menu: ");
+            System.out.println("1 - Add Student");
+            System.out.println("2 - Delete Student");
+            System.out.println("3 - Find Student");
+            System.out.println("4 - Show Students");
+            System.out.println("5 - Exit and Go home");
+            choice = scanner.nextInt();
+        }
     }
 }
